96324df9679221abb2bbc5332f5e941d449bd913
Galen Frechette <galen@thoughtbot.com>
2012-02-13 17:11:23 -0500

    adds various style changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,10 @@ $('a.chapters').toggle(function() {
   $('a.chapters').removeClass("active");
 });
 
+$('a.chapter-selection').toggle(function() {
+  $('.chapter-selector').addClass("expanded");
+  $('a.chapter-selection').addClass("expanded");
+}, function() {
+  $('.chapter-selector').removeClass("expanded");
+  $('a.chapter-selection').removeClass("expanded");
+});
