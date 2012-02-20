1c4060b2936f19a6ffebdc45b41018868e92eb57
Harlow Ward <harlow@apprentice.io>
2012-02-20 17:09:17 -0500

    Guest can view list of trustees on chapter page
    show list of trustees
    rename features file
    rename step definitions file

---
+++ spec/features/step_definitions/guest_views_chapter_steps.rb
@@ -0,0 +1,15 @@
+step 'there are 5 trustees' do
+  @trustees = (1..5).map { FactoryGirl.create(:user) }
+  @trustees.each do |trustee|
+    FactoryGirl.create(:role,
+                       :user => trustee,
+                       :name => 'trustee',
+                       :chapter => @current_chapter)
+  end
+end
+
+step 'I should see the trustees' do
+  @trustees.each do |trustee|
+    page.should have_selector(".trustee-details h3", :text => "#{trustee.first_name} #{trustee.last_name}")
+  end
+end
