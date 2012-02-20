8cc6a8d1d945aa5be595c91957e33a05373d7c79
Jon Yurek <jyurek@thoughtbot.com>
2012-02-20 15:14:07 -0500

    Iron out some of the project home page iteration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @chapters = Chapter.visitable.alphabetically.all
     @projects = Project.recent_winners.limit(10).all
+    @projects = (@projects + ([Project.new] * 15)).first(15)
   end
 end
