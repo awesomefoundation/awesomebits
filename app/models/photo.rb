77139a658dc757880f19f47abf805fa8ff46d3eb
Jon Yurek <jyurek@thoughtbot.com>
2012-02-22 16:39:27 -0500

    Add dimensions to the uploaded images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,11 @@
 class Photo < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :image
+  has_attached_file :image,
+                    :default_style => :main,
+                    :styles => {
+                      :main => "1440x880#",
+                      :index => "500x300#"
+                    }
 
   attr_accessible :image
 end
