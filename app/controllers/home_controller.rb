caba5c4f753b3c00e37ba7a47b7e93021a503189
Galen Frechette <galen@thoughtbot.com>
2012-02-23 12:19:33 -0500

    removes auto filling of empty projects on home page, styles image upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,6 @@
 class HomeController < ApplicationController
   def index
     @chapters = Chapter.visitable.alphabetically.all
-    @projects = fill_out(Project.recent_winners.limit(10).all, 15){ Project.new }
-  end
-
-  private
-
-  def fill_out(array, up_to, &fill_with)
-    (array + (1..up_to).map(&fill_with)).first(up_to)
+    @projects = Project.recent_winners.limit(10).all
   end
 end
