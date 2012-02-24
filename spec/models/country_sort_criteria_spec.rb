acd9965cfdb87cf7bf7f5eaf5e306e2800a8f397
Harlow Ward <harlow@apprentice.io>
2012-02-24 11:21:02 -0500

    renamed the CountrySortCriteria model, updated the spec to run test outside of the let()

--- spec/models/country_sort_criteria_spec.rb
+++ spec/models/country_sort_criteria_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
-describe CountrySorter do
+describe CountrySortCriteria do
 
   let(:chapter1) { FactoryGirl.create(:chapter, :name => 'Salzburg', :country => 'Austria') }
   let(:chapter2) { FactoryGirl.create(:chapter, :name => 'Los Angeles', :country => 'United States') }
   let(:chapter3) { FactoryGirl.create(:chapter, :name => 'Sydney', :country => 'Australia') }
   let(:chapter4) { FactoryGirl.create(:chapter, :name => 'Boston', :country => 'United States') }
-  let(:chapters) { [chapter1, chapter2, chapter3, chapter4].sort_by(&CountrySorter.new(COUNTRY_PRIORITY)) }
+  let(:chapters) { [chapter1, chapter2, chapter3, chapter4] }
 
   context "given an array of chapters" do
     it "should be sorted based on priorty and country name" do
-      chapters.should == [chapter4, chapter2, chapter3, chapter1]
+      chapters.sort_by(&CountrySortCriteria.new(COUNTRY_PRIORITY)).should == [chapter4, chapter2, chapter3, chapter1]
     end
   end
 
