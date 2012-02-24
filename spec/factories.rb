6a9113932f31dfad2ee161acb9595587d70f9182
Harlow Ward <harlow@apprentice.io>
2012-02-24 11:41:55 -0500

    created a :full_chapter factory to clean up the step definitions

--- spec/factories.rb
+++ spec/factories.rb
@@ -48,6 +48,9 @@ FactoryGirl.define do
     description "I am awesome."
     use "I will do awesome."
     chapter
+    factory :full_project do
+      rss_feed_url Rails.root.join('spec', 'support', 'feed.xml').to_s
+    end
   end
 
   factory :vote do
