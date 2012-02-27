934ee4af3250187219f521601496cd934df44d0c
Harlow Ward <harlow@apprentice.io>
2012-02-27 16:21:42 -0500

    Refactored code for I18n translation and cleaner users interface
    updated users javascript
    updated users css for hiding promote/demote divs
    updated en.yml for error translation in app
    updated fetcher class to catch file not found error
    moved typekit.js to top of layout

--- spec/features/step_definitions/admin_manages_promotions_steps.rb
+++ spec/features/step_definitions/admin_manages_promotions_steps.rb
@@ -2,7 +2,7 @@ step 'I promote the trustee to admin' do
   @current_chapter = FactoryGirl.create(:chapter)
   visit chapter_projects_path(@current_chapter)
   click_link("View all Users")
-  page.find(:css, "tr[data-user-id='#{@trustee.id}'].trustee a.admin-toggle").click
+  page.find(:css, "tr[data-user-id='#{@trustee.id}'].non_admin td.promote-demote-admin a.promote-user").click
 end
 
 step 'I should see the new admin' do
