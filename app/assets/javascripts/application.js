e7c279fd1fdb1db08b7819ae5a2b1a1dcab1d0ac
Galen Frechette <galen@thoughtbot.com>
2012-02-28 09:48:22 -0500

    fixes tagline height issue, makes various markup and style changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,13 +25,26 @@ $('a.chapter-selection').toggle(function() {
   $('a.chapter-selection').removeClass("expanded");
 });
 
-$(document).ready( function() {
-  function size_message() {
-    var project_height = $('.project:first-child').outerHeight();
-    $('.tagline').height(project_height - 10);
-  }
+$(document).ready(function() {
 
-  $('.project:first-child img').load(size_message);
+  function size_tagline() {
+    var window_width = $(window).outerWidth() - 10;
 
-  $(window).resize(size_message);
+    if (window_width >= 940) {
+      var inner_width = window_width * 0.2;
+    }
+    else {
+      window_width = 940 - 10;
+      var inner_width = window_width * 0.2;
+    }
+
+    var height = (inner_width - 10) * 0.6;
+    $('.tagline').height(height);
+
+  };
+
+  size_tagline();
+
+  $(window).resize(size_tagline);
+
 });
