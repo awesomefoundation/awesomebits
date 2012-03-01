5db8d52ee838cfedf4b94da733fb1d0211ea6553
Harlow Ward <harlow@apprentice.io>
2012-03-01 16:15:27 -0500

    removed fail state for activerecord destroy

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -3,11 +3,8 @@ class RolesController < ApplicationController
 
   def destroy
     role = Role.find(params[:id])
-    if role.destroy
-      render :json => { :role_id => role.id }
-    else
-      render :json => { :message => I18n.t("users.user.error_removing_user") }, :status => 400
-    end
+    role.destroy
+    render :json => { :role_id => role.id }
   end
 
 end
