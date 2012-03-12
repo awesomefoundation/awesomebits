8f4bc4eceadaeec8b853db5ce35b4c587cb8bec2
Matt Jankowski <mjankowski@thoughtbot.com>
2012-03-12 10:42:06 -0400

    move non-AR classes into separate directory, laving only AR subclasses in app/models

--- config/application.rb
+++ config/application.rb
@@ -18,15 +18,15 @@ end
 module Awesomefoundation
   class Application < Rails::Application
     config.active_record.whitelist_attributes = true
-          config.generators do |generate|
-            generate.test_framework :rspec
-          end
+    config.generators do |generate|
+      generate.test_framework :rspec
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
