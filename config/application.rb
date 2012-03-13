e97261a9dc8a32c4a699563aef04a34f9d9bcaed
Matt Jankowski <mjankowski@thoughtbot.com>
2012-03-13 13:33:48 -0400

    run rake rails:update with 3.2.2

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -17,7 +11,6 @@ end
 
 module Awesomefoundation
   class Application < Rails::Application
-    config.active_record.whitelist_attributes = true
     config.generators do |generate|
       generate.test_framework :rspec
     end
@@ -48,6 +41,17 @@ module Awesomefoundation
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
