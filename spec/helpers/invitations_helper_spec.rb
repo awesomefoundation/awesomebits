a8d82b9574730962cdfd87e290f2e8ee27e08986
Josh Steiner <josh@jsteiner.me>
2012-05-23 14:35:03 -0400

    Sort invitable chapters alphabetically. Fix two tests that weren't testing.

--- spec/helpers/invitations_helper_spec.rb
+++ spec/helpers/invitations_helper_spec.rb
@@ -7,6 +7,8 @@ describe InvitationsHelper do
   let!(:chapter) { role.chapter }
   let!(:another_role) { create(:role, :name => 'dean', :user => user) }
   let!(:another_chapter) { another_role.chapter }
+  let!(:yet_another_role) { create(:role, :name => 'dean', :user => user) }
+  let!(:yet_another_chapter) { yet_another_role.chapter }
   let!(:invitation) { Invitation.new }
 
   describe '#show_chapters_dropdown' do
@@ -17,16 +19,19 @@ describe InvitationsHelper do
   end
 
   describe '#invitable_chapters' do
-    it 'returns an array of chapters available to user' do
+    it 'returns an array of chapters available to user, alphabetically by name' do
       helper.stubs(:current_user).returns(user)
-      helper.invitable_chapters == [chapter, another_chapter]
+      chapter.update_attributes(name: "Alpha")
+      another_chapter.update_attributes(name: "Gamma")
+      yet_another_chapter.update_attributes(name: "Beta")
+      helper.invitable_chapters.should == [chapter, yet_another_chapter, another_chapter]
     end
   end
 
   describe '#primary_invitable_chapter' do
     it 'returns the single chapter available to user' do
       helper.stubs(:current_user).returns(user)
-      helper.primary_invitable_chapter == chapter
+      helper.primary_invitable_chapter.should == chapter
     end
   end
 
