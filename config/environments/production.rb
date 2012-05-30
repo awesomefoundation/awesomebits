11b65b6c1ce5ac133f3a89be2cb56df0bfdff4ca
Jesse Chan-Norris <jcn@pith.org>
2012-05-30 16:39:52 -0400

    Change ActionMailer reference from beta => www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Awesomefoundation::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'beta.awesomefoundation.org' }
+  config.action_mailer.default_url_options = { :host => 'www.awesomefoundation.org' }
 end
