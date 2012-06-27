da3e5c2922a78c9a716629c6a427acd578ae888e
Jesse Chan-Norris <jcn@pith.org>
2012-06-27 15:02:42 +0100

    Change wording of admin 'export all' text to reflect that it takes into account date range

--- spec/features/step_definitions/export_steps.rb
+++ spec/features/step_definitions/export_steps.rb
@@ -5,11 +5,11 @@ step 'I visit a chapters projects page' do
 end
 
 step 'I export all projects' do
-  click_link 'Export All'
+  click_link 'Export All From Date Range'
 end
 
 step 'I should not see the export all link' do
-  page.should_not have_css('p', :text => 'Export All')
+  page.should_not have_css('p', :text => 'Export All From Date Range')
 end
 
 step 'I filter to the last :num_days day(s)' do |num_days|
