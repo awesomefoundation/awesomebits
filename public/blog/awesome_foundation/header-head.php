c12a56357ff2777446a4dc7c39bb5c66c583969d
Jesse Chan-Norris <jcn@pith.org>
2012-07-30 00:15:18 -0400

    Blog: change stylesheet link from staging to www

--- public/blog/awesome_foundation/header-head.php
+++ public/blog/awesome_foundation/header-head.php
@@ -1,5 +1,5 @@
 <head>
   <title><?php bloginfo('name'); ?></title>
-  <link href="http://staging.awesomefoundation.org/assets/application.css" rel="stylesheet" type="text/css" media="screen" />
+  <link href="http://www.awesomefoundation.org/assets/application.css" rel="stylesheet" type="text/css" media="screen" />
   <link href="<?php bloginfo('template_directory'); ?>/stylesheets/screen.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
