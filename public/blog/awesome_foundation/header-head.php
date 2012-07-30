34de4b626f27e8cd68ba3b75376c9d689be81e16
Jesse Chan-Norris <jcn@pith.org>
2012-07-30 00:44:14 -0400

    Blog: title shows name of post and category

--- public/blog/awesome_foundation/header-head.php
+++ public/blog/awesome_foundation/header-head.php
@@ -1,5 +1,5 @@
 <head>
-  <title><?php bloginfo('name'); ?></title>
+  <title><?php wp_title('|',true,'right'); ?> <?php bloginfo('name'); ?></title>
   <link href="http://www.awesomefoundation.org/assets/application.css" rel="stylesheet" type="text/css" media="screen" />
   <link href="<?php bloginfo('template_directory'); ?>/stylesheets/screen.css" rel="stylesheet" type="text/css" media="screen" />
 
