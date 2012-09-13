d22bee64d37714af3b0da6e258d075196a2f85c0
Jesse Chan-Norris <jcn@pith.org>
2012-09-12 17:04:43 -0700

    Add photo dimensions to the application form. Closes #42.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,11 @@
 class Photo < ActiveRecord::Base
+  MAIN_DIMENSIONS = "940x470"
+
   belongs_to :project
   has_attached_file :image,
                     :default_style => :main,
                     :styles => {
-                      :main => "940x470#",
+                      :main => "#{MAIN_DIMENSIONS}#",
                       :index => "500x300#"
                     },
                     :default_url => "/assets/no-image-:style.png"
