c0b3eac7e4761194a511fc94a8e1c1d8712ec952
Jon Pierce <jonpierce@gmail.com>
2012-10-24 22:18:22 -0400

    Implement full text search on projects

---
+++ db/migrate/20121025015059_add_full_text_index_to_projects.rb
@@ -0,0 +1,18 @@
+class AddFullTextIndexToProjects < ActiveRecord::Migration
+
+  COLUMN = [:name, :title, :about_me, :about_project, :use_for_money,
+              :extra_answer_1, :extra_answer_2, :extra_answer_3]
+
+  def up
+    COLUMN.each do |column|
+      execute("create index index_projects_to_tsvector_on_#{column} on projects using gin(to_tsvector('english', #{column}))")
+    end
+  end
+
+  def down
+    COLUMN.each do |column|
+      execute("drop index index_projects_to_tsvector_on_#{column}")
+    end
+  end
+
+end
