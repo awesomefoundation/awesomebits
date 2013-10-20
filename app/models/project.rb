d68322ad7efc9fe7190c06801bd2b009d119c2aa
David Fisher <tibbon@gmail.com>
2013-10-20 16:53:38 -0400

    Updated Project.during_timeframe to pass all tests properly. Some incoming dates were strings and others weren't, so they are now all cast to string before attempting to parse the timezone

--- app/models/project.rb
+++ app/models/project.rb
@@ -47,8 +47,8 @@ class Project < ActiveRecord::Base
   def self.during_timeframe(start_date, end_date)
     start_date = 100.years.ago.strftime('%Y-%m-%d') if start_date.blank?
     end_date   = Time.zone.now.strftime('%Y-%m-%d') if end_date.blank?
-
-    where("projects.created_at BETWEEN ? AND ?", Time.zone.parse(start_date), Time.zone.parse(end_date) + 1.day)
+    binding.pry
+    where("projects.created_at BETWEEN ? AND ?", Time.zone.parse(start_date.to_s), Time.zone.parse(end_date.to_s) + 1.day)
   end
 
   def self.by_vote_count
