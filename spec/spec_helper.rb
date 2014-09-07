cb18ca4183f6aae09b7613ff1a97798afd802e17
Alex Koppel <alex@alexkoppel.com>
2014-09-07 17:16:23 -0400

    Upgrade Turnip

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ Capybara.javascript_driver = :webkit
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/features/step_definitions**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :mocha
