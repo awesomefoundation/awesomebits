014b1a2aa7eac63072d8dc691762ac0dfc8c71f4
Jesse Chan-Norris <jcn@pith.org>
2014-10-19 00:55:51 -0400

    Add cloudfront asset hosts to keep the load off of our dynos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,9 @@ Awesomefoundation::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  if ENV['ASSET_HOST']
+    config.action_controller.asset_host = ENV['ASSET_HOST']
+  end
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
