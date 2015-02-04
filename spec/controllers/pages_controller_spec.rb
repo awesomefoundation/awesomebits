258db8b30ab8bac8ffd073d475981fe1aba6197b
Jesse Chan-Norris <jcn@pith.org>
2015-02-04 00:02:46 -0600

    Add spec to ensure about_us and faq pages render properly

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe HighVoltage::PagesController, '#show' do
+  render_views
+
+  %w(about_us faq).each do |page|
+    %w(en fr pt ru).each do |locale|
+      context "on GET to /#{locale}/#{page}" do
+        before do
+          get :show, id: page, locale: locale
+        end
+
+        it { should respond_with(:success) }
+        it { should render_template(page) }
+      end
+    end
+  end
+end
