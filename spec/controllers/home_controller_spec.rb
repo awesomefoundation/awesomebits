769be1c440ac3d47e43ec1709a74880ea295aa8b
Jesse Chan-Norris <jcn@pith.org>
2015-05-22 23:54:45 -0400

    Homepage country count should only include active countries
    See #94

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,8 +4,8 @@ describe HomeController do
   render_views
 
   context 'displaying the homepage' do
-    let!(:chapter) { FactoryGirl.create(:chapter) }
-    let!(:inactive) { FactoryGirl.create(:inactive_chapter) }
+    let!(:chapter) { FactoryGirl.create(:chapter, :country => "United States") }
+    let!(:inactive) { FactoryGirl.create(:inactive_chapter, :country => "Canada") }
 
     before do
       get :index
@@ -13,6 +13,10 @@ describe HomeController do
 
     it 'should not include inactive chapters in the total chapter count' do
       response.body.should have_selector('section.who-where', :text => '1 Chapter')
+    end
+
+    it 'should not include inactive chapters in the total country count' do
+      response.body.should have_selector('section.who-where', :text => '1 Countr')
     end
 
     it 'should not include inactive chapters in the chapter list' do
