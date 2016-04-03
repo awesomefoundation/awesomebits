ec92a4c376ee16826f0280bbdea21bb6562fe017
Jesse Chan-Norris <jcn@pith.org>
2016-04-03 12:52:36 -0400

    Adds hreflang content to page headers to indicate alternate language versions
    Closes #220

--- spec/views/layout_views_spec.rb
+++ spec/views/layout_views_spec.rb
@@ -1,21 +1,36 @@
 require 'spec_helper'
 
 describe 'layouts/application' do
-  it 'includes the default language locale in the html tag' do
-    view.stubs(:signed_in?).returns(false)
+  context 'the html tag' do
+    it 'includes the default language locale' do
+      view.stubs(:signed_in?).returns(false)
 
-    render
+      render
 
-    rendered.should have_selector('html[lang="en"]')
+      rendered.should have_selector('html[lang="en"]')
+    end
+
+    it 'includes an alternate language locale' do
+      I18n.locale = 'fr'
+
+      view.stubs(:signed_in?).returns(false)
+
+      render
+
+      rendered.should have_selector('html[lang="fr"]')
+    end
   end
 
-  it 'includes an alternate language locale in the html tag' do
-    I18n.locale = 'fr'
+  context 'the header' do
+    it 'includes all of the alternate language versions including itself' do
+      view.stubs(:signed_in?).returns(false)
 
-    view.stubs(:signed_in?).returns(false)
+      render
 
-    render
-
-    rendered.should have_selector('html[lang="fr"]')
+      # Can't figure out why have_selector isn't working here
+      I18n.available_locales.each do |locale|
+        rendered.should include("hreflang=\"#{locale}\" rel=\"alternate\"")
+      end
+    end
   end
 end
