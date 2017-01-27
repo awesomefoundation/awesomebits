0ff8272e9d8c45b77d9a5c377b7f37c697d9b60a
Jesse Chan-Norris <jcn@pith.org>
2017-01-27 14:50:26 -0500

    Update chapter list to remove US & Canada prioritization
    Closes #165

--- spec/models/country_sort_criteria_spec.rb
+++ spec/models/country_sort_criteria_spec.rb
@@ -6,11 +6,12 @@ describe CountrySortCriteria do
   let(:chapter2) { FactoryGirl.create(:chapter, :name => 'Los Angeles', :country => 'United States') }
   let(:chapter3) { FactoryGirl.create(:chapter, :name => 'Sydney', :country => 'Australia') }
   let(:chapter4) { FactoryGirl.create(:chapter, :name => 'Boston', :country => 'United States') }
-  let(:chapters) { [chapter1, chapter2, chapter3, chapter4] }
+  let(:chapter5) { FactoryGirl.create(:chapter, :name => 'Awesome Without Borders', :country => 'Worldwide') }
+  let(:chapters) { [chapter1, chapter2, chapter3, chapter4, chapter5] }
 
   context "given an array of chapters" do
     it "should be sorted based on priorty and country name" do
-      chapters.sort_by(&CountrySortCriteria.new(COUNTRY_PRIORITY)).should == [chapter4, chapter2, chapter3, chapter1]
+      chapters.sort_by(&CountrySortCriteria.new(COUNTRY_PRIORITY)).should == [chapter5, chapter3, chapter1, chapter4, chapter2]
     end
   end
 
