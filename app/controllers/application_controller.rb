32de071bfd45c3e8e934131075cf01c6dd600e86
Jesse Chan-Norris <jcn@pith.org>
2017-09-17 13:17:15 -0400

    If an invalid locale is passed in, use the default locale
    https://rollbar.com/awesomefoundation/awesomefoundation/items/11/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,11 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    begin
+      I18n.locale = params[:locale] || I18n.default_locale
+    rescue I18n::InvalidLocale
+      I18n.locale = I18n.default_locale
+    end
   end
 
   def default_url_options(options={})
