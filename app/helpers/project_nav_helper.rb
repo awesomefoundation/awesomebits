9d1e38f8e9b23ab4cac786a2f257a307f7833671
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2018-08-07 11:43:33 +0600

    Fix code style (parenthesized method arguments)

--- app/helpers/project_nav_helper.rb
+++ app/helpers/project_nav_helper.rb
@@ -1,6 +1,6 @@
 module ProjectNavHelper
 
-  def next_project_id project
+  def next_project_id(project)
     return if is_last_project? project
 
     all_keys = chapter_projects_keys(project)
@@ -8,7 +8,7 @@ module ProjectNavHelper
     all_keys[index_next_project]
   end
 
-  def prev_project_id project
+  def prev_project_id(project)
     return if is_first_project? project
 
     all_keys = chapter_projects_keys(project)
@@ -22,11 +22,11 @@ module ProjectNavHelper
     project.chapter.projects.order('funded_on DESC').pluck(:id)
   end
 
-  def is_last_project? project
+  def is_last_project?(project)
     chapter_projects_keys(project).last == project.id
   end
 
-  def is_first_project? project
+  def is_first_project?(project)
     chapter_projects_keys(project).first == project.id
   end
 end
