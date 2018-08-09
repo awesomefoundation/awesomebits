a33df45b44837e4432942584ef1f2c200d29d476
Ilya Konanykhin <ilya.konanykhin@gmail.com>
2018-08-09 14:30:49 +0600

    Remove private method tests

--- spec/helpers/project_nav_helper.rb
+++ spec/helpers/project_nav_helper.rb
@@ -40,34 +40,4 @@ describe ProjectNavHelper do
     end
   end
 
-  context 'private methods' do
-    let(:chapter) { FactoryGirl.create :chapter, :name => 'Test chapter' }
-    let!(:first_project) { FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today + 1 }
-    let!(:last_project) { FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today }
-
-    context 'is_first_project?' do
-      it 'The first project is the first' do
-        expect(helper.send(:is_first_project?, first_project)).to be_truthy
-      end
-
-      it 'The last project is not the first one' do
-        expect(helper.send(:is_first_project?, last_project)).to be_falsey
-      end
-    end
-
-    context 'is_last_project?' do
-      it 'The last project is the last one' do
-        expect(helper.send(:is_last_project?, last_project)).to be_truthy
-      end
-      it 'The last project is not the first one' do
-        expect(helper.send(:is_last_project?, first_project)).to be_falsey
-      end
-    end
-
-    context 'chapter_projects_keys' do
-      it 'The correct sorting of keys in the chapter_projects_keys method' do
-        expect(helper.send(:chapter_projects_keys, first_project)).to eq([first_project.id, last_project.id])
-      end
-    end
-  end
 end
