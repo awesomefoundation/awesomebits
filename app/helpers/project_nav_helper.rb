adecab87c34240bbe934f001118819094ebd4d2f
Jesse Chan-Norris <jcn@pith.org>
2018-08-25 01:22:48 -0400

    More efficient next/previous links and more readable tests
    * No longer fetch and instantiate every project just to get next and
      previous; use 2 small queries instead
    * Reorganize the tests and utilize better language for readability

--- app/helpers/project_nav_helper.rb
+++ app/helpers/project_nav_helper.rb
@@ -1,18 +1,8 @@
 module ProjectNavHelper
   def project_winning_siblings(project)
-    all_projects = chapter_winning_projects(project).to_a
-    project_index = all_projects.index { |obj| obj.id == project.id }
-    return [nil, nil] unless project_index
-
-    prev_project = all_projects[project_index - 1] unless project_index.zero?
-    next_project = all_projects[project_index + 1] unless project_index == all_projects.count - 1
+    prev_project = project.chapter.winning_projects.where(Project.arel_table[:funded_on].gt(project.funded_on)).last
+    next_project = project.chapter.winning_projects.where(Project.arel_table[:funded_on].lt(project.funded_on)).first
 
     [prev_project, next_project]
-  end
-
-  private
-
-  def chapter_winning_projects(project)
-    project.chapter.winning_projects.order('funded_on DESC')
   end
 end
--- spec/helpers/project_nav_helper.rb
+++ spec/helpers/project_nav_helper.rb
@@ -4,46 +4,49 @@ require 'date'
 describe ProjectNavHelper do
   describe '#project_winning_siblings' do
     let(:chapter) { FactoryGirl.create :chapter, :name => 'Test chapter' }
-    let!(:project) { FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today + 1 }
+    let!(:project) { FactoryGirl.create :winning_project, :chapter => chapter }
 
-    it 'returns no siblings for single project' do
-      expect(helper.project_winning_siblings(project).compact).to be_empty
+    context 'for a single winning project' do
+      it 'returns no siblings' do
+        expect(helper.project_winning_siblings(project).compact).to be_empty
+      end
+
+      it 'does not include not winning projects as siblings' do
+        not_winning_projects = FactoryGirl.create_list :project, 2, :chapter => chapter
+        expect(helper.project_winning_siblings(project).compact).to be_empty
+      end
     end
 
-    it 'returns no siblings for not winning project' do
-      not_winning_project = FactoryGirl.create :project, :chapter => chapter
-      expect(helper.project_winning_siblings(not_winning_project).compact).to be_empty
+    context 'for a single not winning project' do
+      it 'returns no siblings' do
+        not_winning_project = FactoryGirl.create :project, :chapter => chapter
+        expect(helper.project_winning_siblings(not_winning_project).compact).to be_empty
+      end
     end
 
-    it 'does not include not winning projects as siblings' do
-      not_winning_projects = FactoryGirl.create_list :project, 2, :chapter => chapter
-      expect(helper.project_winning_siblings(project).compact).to be_empty
-    end
+    context 'with winning projects' do
+      let!(:next_project)     { FactoryGirl.create :project, :chapter => chapter, :funded_on => project.funded_on - 1.day }
+      let!(:previous_project) { FactoryGirl.create :project, :chapter => chapter, :funded_on => project.funded_on + 1.day }
 
-    it 'returns Next sibling for first project' do
-      second_project = FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today
-      expect(helper.project_winning_siblings(project).last).to eq(second_project)
-    end
+      it 'returns Next sibling for first project' do
+        expect(helper.project_winning_siblings(previous_project).last).to eq(project)
+      end
 
-    it 'returns Prev sibling for second project' do
-      second_project = FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today
-      expect(helper.project_winning_siblings(second_project).first).to eq(project)
-    end
+      it 'returns no Prev sibling for first project' do
+        expect(helper.project_winning_siblings(previous_project).first).to be_nil
+      end
 
-    it 'returns no Next sibling for last project' do
-      second_project = FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today
-      expect(helper.project_winning_siblings(second_project).last).to be_nil
-    end
+      it 'returns Prev sibling for last project' do
+        expect(helper.project_winning_siblings(next_project).first).to eq(project)
+      end
 
-    it 'returns no Prev sibling for first project' do
-      second_project = FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today
-      expect(helper.project_winning_siblings(project).first).to be_nil
-    end
+      it 'returns no Next sibling for last project' do
+        expect(helper.project_winning_siblings(next_project).last).to be_nil
+      end
 
-    it 'returns Prev, Next siblings' do
-      second_project = FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today
-      third_project  = FactoryGirl.create :project, :chapter => chapter, :funded_on => Date.today - 1
-      expect(helper.project_winning_siblings(second_project)).to eq([project, third_project])
+      it 'returns Prev, Next siblings' do
+        expect(helper.project_winning_siblings(project)).to eq([previous_project, next_project])
+      end
     end
   end
 end
