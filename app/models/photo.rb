59dbaf477a2f7312df89086ecfc177fae6ec3268
Jesse Chan-Norris <jcn@pith.org>
2019-05-02 16:06:26 -0400

    Remove old Photo cropped_image_url support code

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -68,21 +68,8 @@ class Photo < ApplicationRecord
   end
 
   def cropped_image_url(crop)
-    # [image_host, "q", image_path(crop)].join("/")
-
     cropper = ENV['THUMBOR_HOST'] ? :thumbor : :magickly
     ImageCropper.new(cropper, self).crop_url(crop)
-  end
-
-  def image_host
-    ENV['MAGICKLY_HOST']
-  end
-
-  def image_path(crop)
-    # We unescape before re-escaping because the URL that comes from Paperclip is already
-    # escaped, so without unescaping, we'd be double-escaping the URL, which causes problems
-    # especially with UTF-8 encoded filenames.
-    [ "src", URI.unescape(original_url), "output", "jpg", "convert", "-auto-orient", "thumb", crop ].collect { |part| CGI.escape(part) }.join("/")
   end
 
   def fog
