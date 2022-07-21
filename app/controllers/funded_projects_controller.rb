6daf09ab26ab1cafe5e16df84d9d59cfb4f10ac5
Jesse Chan-Norris <jcn@pith.org>
2022-07-21 16:36:27 -0400

    Add a new /projects page with very basic filtering

--- app/controllers/funded_projects_controller.rb
+++ app/controllers/funded_projects_controller.rb
@@ -1,10 +1,22 @@
 class FundedProjectsController < ApplicationController
   def index
-    @projects = Project.includes(:real_photos, :chapter).winners.order("funded_on DESC").paginate(:page => params[:page], :per_page => 50)
+    @projects = Project.preload(:primary_photo, :chapter).winners.order(funded_on: :desc)
+
+    if params[:q].present?
+      @projects = @projects.search(params[:q]) if params[:q].present?
+    end
+
+    if params[:chapter].present?
+      @projects = @projects.joins(:chapter).merge(Chapter.where(slug: params[:chapter]))
+    end
+
+    @projects = @projects.paginate(page: params[:page], per_page: 52)
 
     respond_to do |format|
       format.xml
-      format.html { render_404 }
+      format.html do
+        @chapters = Chapter.joins(:winning_projects).distinct.select_data(:all, include_any: false).collect { |c| [c.name, c.chapters.collect { |chapter| [chapter.name, chapter.slug] }] }
+      end
     end
   end
 end
